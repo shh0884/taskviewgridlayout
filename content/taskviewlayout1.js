65f54857b420da9b6dc6229f587a43cc96696d50
shh0884 <51844838+shh0884@users.noreply.github.com>
2019-09-17 21:08:14 +0900

    Update taskviewlayout1.js

--- content/taskviewlayout1.js
+++ content/taskviewlayout1.js
@@ -1,14 +1,12 @@
-var insertBefore = 'document.getElementById('calendar-task-box').insertBefore(document.getElementById('calendar-task-details-container'), document.getElementById('task-addition-box'))';
+browser.menus.onClicked.addListener((info, tab) => {
+var insertBefore = document.getElementById('calendar-task-box').insertBefore(document.getElementById('calendar-task-details-container'), document.getElementById('task-addition-box'));
 var InsertBefore = browser.tabs.executeScript({
          code: 'insertBefore'
 });
-var deckStyle1 = 'document.getElementById('calendarDisplayDeck').style.flex = '1'';
+var deckStyle1 = document.getElementById('calendarDisplayDeck').style.flex = '1';
 var DeckStyle1 = browser.tabs.executeScript({
          code: 'deckStyle1'
 });
-      var ButtonListener1 = 'document.querySelector('taskview1').addEventListener('click', function() {InsertBefore; DeckStyle1;})';
-      var ButtonLIstener1 = browser.tabs.executeScript({
-         code: 'ButtonLIstener1'
-      });
-   
-      ButtonLIstener2.then();
+ InsertBefore;
+ DeckStyle1;
+});
