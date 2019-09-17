c77ac68ed5a7a48e4436c3291516b05ddd0dec9e
shh0884 <51844838+shh0884@users.noreply.github.com>
2019-09-17 21:09:41 +0900

    Update taskviewlayout3.js

--- content/taskviewlayout3.js
+++ content/taskviewlayout3.js
@@ -1,15 +1,12 @@
-var deckStyle3 = 'document.getElementById('calendarDisplayDeck').style.flex = '0 2 0'';
+browser.menus.onClicked.addListener((info, tab) => {
+var deckStyle3 = document.getElementById('calendarDisplayDeck').style.flex = '0 2 0';
 var DeckStyle3 = browser.tabs.executeScript({
          code: 'deckStyle3'
 });
-var appendChild3 = 'document.getElementById('calendarContent').appendChild(document.getElementById('calendar-task-details-container'))';
+var appendChild3 = document.getElementById('calendarContent').appendChild(document.getElementById('calendar-task-details-container'));
 var AppendChild3 = browser.tabs.executeScript({
          code: 'appendChild3'
 });
-
-      var ButtonListener3 = 'document.querySelector('taskview3').addEventListener('click', function() {DeckStyle3; AppendChild3;})';
-      var ButtonLIstener3 = browser.tabs.executeScript({
-         code: 'ButtonLIstener3'
-      });
-   
-      ButtonLIstener3.then();
+ DeckStyle3;
+ appendChild3;
+});
