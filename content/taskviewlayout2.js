1d5249afeeb45e6e6052c64fa9bcb515b0f472a5
shh0884 <51844838+shh0884@users.noreply.github.com>
2019-09-17 13:16:36 +0900

    Update taskviewlayout2.js

--- content/taskviewlayout2.js
+++ content/taskviewlayout2.js
@@ -1,36 +1,15 @@
-browser.menus.onClicked.addListener(function(info, tab) {
-      var button = document.createElement('button');
-      var Button = browser.tabs.executeScript({
-         code: 'button'
-      });      
-      var buttonChild = document.activeElement.appendChild(button);
-      var ButtonChild = browser.tabs.executeScript({
-         code: 'buttonChild'
-      }); 
-      var Taskview2 = Button.setAttribute('id', 'taskview2'); 
-      var taskView2 = browser.tabs.executeScript({
-         code: 'Taskview2'
-      });              
-      if(info.menuItemId === taskView2) {
-      var layoutTBox = document.getElementById('calendar-task-box');
-      var LayoutTBox = browser.tabs.executeScript({
-          code: 'layoutTBox'
-      });
-      var layoutContainer = document.getElementById('calendar-task-details-container');
-      var LayoutContainer = browser.tabs.executeScript({
-          code: 'layoutContainer'
-      });
-      var layoutDeck = document.getElementById('calendarDisplayDeck');
-      var LayoutDeck = browser.tabs.executeScript({
-         code: 'layoutDeck'
-      });
-      var LayoutChild = LayoutTBox.appendChild = LayoutContainer; 
-      var LayoutCHild = browser.tabs.executeScript({
-         code: 'LayoutChild'
-      });
-      LayoutCHild;
-      var flex = 'layoutDeck{flex: 1 }';
-      LayoutDeck.insertCSS = flex;
-}
+var appendChild = window.document.getElementById('calendar-task-box').appendChild(document.getElementById('calendar-task-details-container'));
+var AppendChild = browser.tabs.executeScript({
+code: 'appendChild'
+});
+var deckStyle = document.getElementById('calendarDisplayDeck').style.flex = '1';
+var DeckStyle = browser.tabs.executeScript({
+code: 'deckStyle'
 });
 
+var ButtonListener2 = 'document.querySelector('taskview2').addEventListener('click', function() {AppendChild; DeckStyle;})';
+var ButtonLIstener2 = browser.tabs.executeScript({
+code: 'ButtonLIstener2'
+});
+   
+      ButtonLIstener2.then();
