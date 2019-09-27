18029971328010be96befb6ce7eec508dc39bb95
shh0884 <51844838+shh0884@users.noreply.github.com>
2019-09-27 20:09:00 +0900

    Update tv1.js

--- content/tv1.js
+++ content/tv1.js
@@ -1,3 +1,3 @@
-document.getElementById('div1').addEventListener('click', function() {window.addEventListener("DOMContentLoaded", function(event){
+document.getElementById('div1').addEventListener('click', function() {window.addEventListener('DOMContentLoaded', function(event){
 console.log(parent.document.getElementById('calendar-task-box').insertBefore(parent.document.getElementById('calendar-task-details-container')), parent.document.getElementById('calendarDisplayDeck').style.flex = '1')
 })})
