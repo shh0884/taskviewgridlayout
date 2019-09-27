7f0ab01b5aec56366caa98ce8dad66cd52e4b57a
shh0884 <51844838+shh0884@users.noreply.github.com>
2019-09-27 20:04:21 +0900

    Update and rename taskviewlayout2.js to tv2.js

---
+++ content/tv2.js
@@ -0,0 +1,3 @@
+document.getElementById('div2').addEventListener('click', function() {window.addEventListener("DOMContentLoaded", function(event){
+console.log(parent.document.getElementById('calendar-task-box').appendChild(parent.document.getElementById('calendar-task-details-container')), parent.document.getElementById('calendarDisplayDeck').style.flex = '1')
+})})
