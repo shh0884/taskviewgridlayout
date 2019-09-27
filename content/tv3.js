37575e96afc0886763e6e037bbdd9165c3ca9d8a
shh0884 <51844838+shh0884@users.noreply.github.com>
2019-09-27 20:10:02 +0900

    Update tv3.js

--- content/tv3.js
+++ content/tv3.js
@@ -1,3 +1,3 @@
-document.getElementById('div3').addEventListener('click', function() {window.addEventListener("DOMContentLoaded", function(event){
+document.getElementById('div3').addEventListener('click', function() {window.addEventListener('DOMContentLoaded', function(event){
 console.log(parent.document.getElementById('calendarDisplayDeck').style.flex = '0 2 0', parent.document.getElementById('calendarContent').appendChild(parent.document.getElementById('calendar-task-details-container')))
 })})
