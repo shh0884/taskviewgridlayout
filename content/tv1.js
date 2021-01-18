7aec5a7fc3dee9f9bfdae39b45c8892e3f8a1c33
shh0884 <51844838+shh0884@users.noreply.github.com>
2021-01-18 15:26:44 +0900

    Delete content directory

--- content/tv1.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('div1').addEventListener('click', function() {window.addEventListener('DOMContentLoaded', function(event){
-console.log(parent.document.getElementById('calendar-task-box').insertBefore(parent.document.getElementById('calendar-task-details-container')), parent.document.getElementById('calendarDisplayDeck').style.flex = '1')
-})})
--- content/tv4.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('div4').addEventListener('click', function() {window.addEventListener('DOMContentLoaded', function(event){
-console.log(parent.document.getElementById('calendar-task-box').appendChild(parent.document.getElementById('calendar-task-details-container')), parent.document.getElementById('calendarDisplayDeck').style.flex = '1')
-})})
